2570d3eb63a33bfe507587cf45ddb17499abc56d
Xiaochuang Han <xc@gatech.edu>
2017-10-16 19:23:02 -0400

    tut3

---
+++ tut3.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+db = MongoClient('mongodb://143.215.138.132:27017')['big_data']
+
+limit = {'$limit': 10000}
+
+pipeline = [limit]
+
+author_dict = {}
+for tweet in db.tweet_subset.aggregate(pipeline):
+    author = tweet['author_name']
+    if author in author_dict:
+        author_dict[author] += 1
+    else:
+        author_dict[author] = 1
+
+print sorted(author_dict, key=lambda x:author_dict[x], reverse=True)[0]
